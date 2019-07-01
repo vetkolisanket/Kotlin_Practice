19dd59dcfb72ce8894d6785f8929fd60ef965e45
Sanket <vetkolisanket@gmail.com>
2019-07-01 10:30:40 +0530

    eg showing invoking the value of a function

--- src/FunctionInstantiation.kt
+++ src/FunctionInstantiation.kt
@@ -7,6 +7,13 @@ val repeatFun: String.(Int) -> String = {times -> this.repeat(times)}
 val twoParameters: (String, Int) -> String = repeatFun
 
 /**
+ * If the value has a receiver type, the receiver object should be passed as the first argument. Another way to invoke a
+ * value of a function type with receiver is to prepend it with the receiver object, as if the value were an extension function:
+ */
+val stringPlus: (String, String) -> String = String::plus
+val intPlus: Int.(Int) -> Int = Int::plus
+
+/**
  * You can use either of repeatFun or twoParameters as an argument to runTransformation
  */
 fun runTransformation(f: (String, Int) -> String): String {
@@ -14,6 +21,14 @@ fun runTransformation(f: (String, Int) -> String): String {
 }
 
 fun main() {
+//    val result = runTransformation(repeatFun)
     val result = runTransformation(twoParameters)
     println(result)
+
+    println(stringPlus.invoke("<-", "->"))
+    println(stringPlus("Hello, ", "world!"))
+
+    println(intPlus.invoke(1, 1))
+    println(intPlus(1, 2))
+    println(2.intPlus(3)) // extension-like call
 }
