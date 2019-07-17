89955102d08ec2ba40417088c2e6ba6a499e54c8
vetkolisanket <vetkolisanket@gmail.com>
2019-07-17 10:38:00 +0530

    Added shop and its related data classes

---
+++ src/collections/Shop.kt
@@ -0,0 +1,17 @@
+package collections
+
+data class City(val name: String) {
+    override fun toString() = name
+}
+
+data class Product(val name: String, val price: Double) {
+    override fun toString() = "'$name' for '$price'"
+}
+
+data class Order(val products: List<Product>, val isDelivered: Boolean)
+
+data class Customer(val name: String, val city: City, val orders: List<Order>) {
+    override fun toString() = "$name from ${city.name}"
+}
+
+data class Shop(val name: String, val customers: List<Customer>)
