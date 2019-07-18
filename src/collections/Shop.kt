329dda16adfe8b171c1fe735b8c348bf98aa414f
vetkolisanket <vetkolisanket@gmail.com>
2019-07-18 22:56:40 +0530

    Added many collection predicates

--- src/collections/Shop.kt
+++ src/collections/Shop.kt
@@ -16,4 +16,23 @@ data class Customer(val name: String, val city: City, val orders: List<Order>) {
 
 data class Shop(val name: String, val customers: List<Customer>)
 
+//Return set of customers
 fun Shop.getSetOfCustomers(): Set<Customer> = customers.toSet()
+
+//Return the set of cities the customers are from
+fun Shop.getCitiesOfCustomers(): Set<City> = customers.map { it.city }.toSet()
+
+//Return a list of customers who live in the given city
+fun Shop.getCustomersFrom(city: City): List<Customer> = customers.filter { it.city == city }
+
+//Return true if all customers are from the given city
+fun Shop.checkAllCustomersAreFrom(city: City): Boolean = customers.all { it.city == city }
+
+//Return true if there is at least one customer from the given city
+fun Shop.hasCustomerFrom(city: City): Boolean = customers.any { it.city == city }
+
+//Return the number of customers from the given city
+fun Shop.countCustomersFrom(city: City): Int = customers.count { it.city == city }
+
+//Return a customer who lives in the given city, or null if there is none
+fun Shop.findAnyCustomerFrom(city: City): Customer? = customers.find { it.city == city }
