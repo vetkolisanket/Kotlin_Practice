4ea69fdfa7255754dee59ed115d0bfacb0a3638e
vetkolisanket <vetkolisanket@gmail.com>
2019-07-19 19:57:38 +0530

    Added flat map example

--- src/collections/Shop.kt
+++ src/collections/Shop.kt
@@ -36,3 +36,17 @@ fun Shop.countCustomersFrom(city: City): Int = customers.count { it.city == city
 
 //Return a customer who lives in the given city, or null if there is none
 fun Shop.findAnyCustomerFrom(city: City): Customer? = customers.find { it.city == city }
+
+//Return all products this customer has ordered
+val Customer.orderedProducts: Set<Product> get() {
+    return orders.flatMap {
+        it.products
+    }.toSet()
+}
+
+//Return all products that were ordered by at least one customer
+val Shop.allOrderedProducts: Set<Product> get() {
+    return customers.flatMap {
+        it.orderedProducts
+    }.toSet()
+}
