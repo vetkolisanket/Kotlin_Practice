39db4215e1993dac6a6879103526a3129e82fdeb
vetkolisanket <vetkolisanket@gmail.com>
2019-07-20 10:22:20 +0530

    Added maxBy example

--- src/collections/Shop.kt
+++ src/collections/Shop.kt
@@ -50,3 +50,9 @@ val Shop.allOrderedProducts: Set<Product> get() {
         it.orderedProducts
     }.toSet()
 }
+
+//Return a customer whose order count is the highest among all customers
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers.maxBy { it.orders.size }
+
+//Return the most expensive product which has been ordered
+fun Customer.getMostExpensiveOrderedProduct(): Product? = orders.flatMap { it.products }.maxBy { it.price }
