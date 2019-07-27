2ddcaa5946b8b12cd940c7fbf488258a54faa244
vetkolisanket <vetkolisanket@gmail.com>
2019-07-27 12:55:47 +0530

    Added a practice example of collection extension functions

---
+++ src/collections/Practice.kt
@@ -0,0 +1,11 @@
+package collections
+
+fun doSomethingStrangeWithCollections(collection: Collection<String>): Collection<String>? {
+
+    val groupsByLength = collection.groupBy { it.length }
+
+    val maximumSizeOfGroup = groupsByLength.values.map { it.size }.max()
+
+    return groupsByLength.values.firstOrNull{ it.size == maximumSizeOfGroup }
+
+}
