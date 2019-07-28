6ae8134f6932ce4e19e4f5e06e0563087749dbd0
vetkolisanket <vetkolisanket@gmail.com>
2019-07-28 15:37:13 +0530

    Added custom lazy property example

--- src/properties/Properties.kt
+++ src/properties/Properties.kt
@@ -4,8 +4,23 @@ package properties
 class PropertyExample() {
     var counter = 0
     var propertyWithCounter: Int? = null
-    set(value) {
-        field = value
-        counter++
-    }
+        set(value) {
+            field = value
+            counter++
+        }
+}
+
+//Custom lazy property
+class LazyProperty(val initializer: () -> Int) {
+
+    var value: Int? = null
+
+    val lazy: Int
+        get() {
+            if (value == null) {
+                value = initializer()
+            }
+            return value!!
+        }
+
 }
