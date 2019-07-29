1fee63d60676ae1755bdc2621136d9a3909bd031
vetkolisanket <vetkolisanket@gmail.com>
2019-07-29 21:10:41 +0530

    Added vetoable example

--- src/properties/Observable.kt
+++ src/properties/Observable.kt
@@ -6,10 +6,30 @@ class User {
     var name: String by Delegates.observable("<no name>") {
         property, oldValue, newValue -> println("$property from $oldValue -> $newValue")
     }
+
+    var age: Int by Delegates.vetoable(0) {
+        property, oldValue, newValue ->
+        if (newValue < 0) {
+            println("Can't assign negative value for age")
+            false
+        } else {
+            println("$property from $oldValue -> $newValue")
+            true
+        }
+    }
+
+    override fun toString(): String {
+        return "Name: $name, Age: $age"
+    }
 }
 
 fun main() {
     val user = User()
     user.name = "first"
     user.name = "second"
+
+    user.age = 55
+    user.age = -55
+
+    println(user)
 }
