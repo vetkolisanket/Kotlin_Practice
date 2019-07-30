ad698b53a875c0d2daa179221a298528c9b74042
vetkolisanket <vetkolisanket@gmail.com>
2019-07-30 22:17:09 +0530

    Added delegates example

---
+++ src/properties/Delegates.kt
@@ -0,0 +1,39 @@
+package properties
+
+import java.util.*
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+
+fun MyDate.toMillis(): Long {
+    val c = Calendar.getInstance()
+    c.set(year, month, dayOfMonth, 0,0,0)
+    c.set(Calendar.MILLISECOND, 0)
+    return c.timeInMillis
+}
+
+fun Long.toDate(): MyDate {
+    val c = Calendar.getInstance()
+    c.timeInMillis = this
+    return MyDate(c[Calendar.YEAR], c[Calendar.MONTH], c[Calendar.DAY_OF_MONTH])
+}
+
+class D {
+    var date: MyDate by EffectiveDate()
+}
+
+class EffectiveDate<R> : ReadWriteProperty<R, MyDate> {
+
+    var timeInMillis: Long? = null
+
+    override fun getValue(thisRef: R, property: KProperty<*>): MyDate {
+        return timeInMillis!!.toDate()
+    }
+
+    override fun setValue(thisRef: R, property: KProperty<*>, value: MyDate) {
+        timeInMillis = value.toMillis()
+    }
+
+
+}
