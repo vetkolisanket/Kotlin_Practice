b026358da62767c04f58e0acec503652ae24c206
vetkolisanket <vetkolisanket@gmail.com>
2019-08-01 23:06:21 +0530

    Added builder example using extension function

--- src/builder/Builder.kt
+++ src/builder/Builder.kt
@@ -6,3 +6,22 @@ fun task(): List<Boolean> {
 
     return listOf(42.isOdd(), 239.isOdd(), 233524234.isEven())
 }
+
+fun usage(): Map<Int, String> {
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
+}
+
+fun buildMap(build: HashMap<Int, String>.() -> Unit): HashMap<Int, String> {
+    val map = hashMapOf<Int, String>()
+    map.build()
+    return map
+}
+
+fun main() {
+    print(usage())
+}
